92ba5c77b6ae7fa161407338e251ecdb84ecc21c
Li-Pro <chris.chou.c@nycu.edu.tw>
2022-02-20 23:53:05 +0800

    Countline.py reads from $2, and fallbacks to __file__ for compatibility.
    Rebase:
    
    - Pass 2nd args into countline.py.
    
    - Fix countline.py to read from $2.
    
    - Fix mistyped `srcfile` in countline.py.
    
    - Fix mistyped argv #.

--- hw1/Li-Pro/countline.py
+++ hw1/Li-Pro/countline.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env -S bash -c 'exec $0 $1' ${PYTHON_BIN}
+#!/usr/bin/env -S bash -c 'exec $0 $1 $2' ${PYTHON_BIN}
 
-#                                           
 #           QUICK AND DIRTY CODE            
 #                                           
 #  T   T       Q   Q                        
 #    u           U                          
 #                                           
 #  That's the evilest thing I can imagine.  
-#                                           
+import sys
 
-with open(__file__, 'r') as srcfile:
-    content = srcfile.read()
-    print('{}\n'.format(content.count('\n')))
+from_file = sys.argv[1]  if len(sys.argv) >= 2  else __file__
+
+with open(from_file, 'r') as srcfile:
+    print('{}\n'.format(srcfile.read().count('\n')))
