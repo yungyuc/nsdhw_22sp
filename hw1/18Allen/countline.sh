cec2bc6669f2f0dd6ad22e2d3273f7ed591e14ab
18Allen <allen891118@gmail.com>
2022-02-21 23:55:20 +0800

    Add countline.py and countline.sh under 18Allen folder

---
+++ hw1/18Allen/countline.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+if [ $# -lt 1 ]; then
+    echo 'missing file name'
+elif [ $# -gt 1 ]; then
+    echo 'only one argument is allowed'
+else
+    if [ -e $1 ]; then
+        echo "$(wc -l < $1) lines in $1"
+    else 
+        echo "$1 does not exists"
+    fi
+fi
