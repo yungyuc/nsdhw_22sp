e506cb2cdc6581fadcc0d75834827fa4fe3a7ce1
minchincho <violacho1002@gmail.com>
2022-02-22 16:51:20 +0800

    [hw1] finished question 2: rewrite countline.py to switch python executables

---
+++ hw1/minchuncho/countline.py
@@ -0,0 +1,22 @@
+if [[ $# < 1 ]] ; then
+    echo -e "missing file name"
+elif [[ $# > 1 ]] ; then
+    echo -e "only one argument is allowed"
+else
+    export FNAME=$1
+fi
+
+exec ${PB} << HERE
+
+import sys
+import os
+
+fname = os.environ.get("FNAME")
+if os.path.exists(fname):
+    with open(fname) as fobj:
+        lines = fobj.readlines()
+    sys.stdout.write('{} lines in {}\n'.format(len(lines), fname))
+else:
+    sys.stdout.write('{} not found\n'.format(fname))
+
+HERE
