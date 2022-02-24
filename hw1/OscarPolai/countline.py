3d9e5dd6286c6a771e72b3586568d2ad9bd6340f
OscarPolai <seizetheday987456321@gmail.com>
2022-02-24 17:13:48 +0800

    Finish all requirements of hw1

---
+++ hw1/OscarPolai/countline.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import sys
+import os.path
+
+env = os.environ['PYTHON_BIN']
+if env == 'python0': 
+    sys.exit("exec: python0: not found")
+
+if len(sys.argv) < 2:
+    sys.stdout.write('missing file name\n')
+elif len(sys.argv) > 2:
+    sys.stdout.write('only one argument is allowed\n')
+else:
+    fname = sys.argv[1]
+    if os.path.exists(fname):
+        with open(fname) as fobj:
+            lines = fobj.readlines()
+        sys.stdout.write('{} lines in {}\n'.format(len(lines), fname))
+    else:
+        sys.stdout.write('{} not found\n'.format(fname))
+
