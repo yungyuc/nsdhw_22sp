923e3f814546af61edaff592cedbc9d628327722
YuChiehLiu <a0926858615@gmail.com>
2022-02-25 17:04:16 +0800

    question1&2 all done

---
+++ hw1/YuChiehLiu/countline.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import sys
+import os
+
+python_version = os.environ.get("PYTHON_BIN")
+
+if python_version == "python0":
+    print("exec: python0: not found")
+    sys.exit(1)
+
+if len(sys.argv) < 2:
+    sys.stdout.write('missing file name\n')
+elif len(sys.argv) > 2:
+    sys.stdout.write('only one argument is allowed\n')
+else:
+    fname = sys.argv[1]
+    if os.path.exists(fname):
+        with open(fname) as fobj:
+            lines = fobj.readlines()
+        sys.stdout.write('{} lines in {}\n'.format(len(lines), fname))
+    else:
+        sys.stdout.write('{} not found\n'.format(fname))
