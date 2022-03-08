dc8279eee065ecc4e3b5693b97478cc88b0be6a7
BlenderWang9487 <developinblend@gmail.com>
2022-03-08 20:19:46 +0800

    q1 action test

--- hw2/BlenderWang9487/q1/Line.cpp
+++ hw2/BlenderWang9487/q1/Line.cpp
@@ -29,3 +29,4 @@ int main(int, char **)
 
     return 0;
 }
+
--- hw2/BlenderWang9487/q1/Line.hpp
+++ hw2/BlenderWang9487/q1/Line.hpp
@@ -1,21 +1,50 @@
 #pragma once
 #include <vector>
+#include <iostream>
 
 class Line
 {
-public:
-    Line();
-    Line(Line const & );
-    Line(Line       &&);
-    Line & operator=(Line const & );
-    Line & operator=(Line       &&);
-    Line(size_t size);
-    ~Line();
-    size_t size() const;
-    float & x(size_t it) const;
-    float & x(size_t it);
-    float & y(size_t it) const;
-    float & y(size_t it);
-private:
-    // Member data.
-}; /* end class Line */
+  private:
+    std::vector<float> _x;
+    std::vector<float> _y;
+  public:
+    Line(){}
+    Line(Line const &  l):_x(l._x),_y(l._y){}
+    Line(Line       && l):
+        _x(std::forward<Line>(l)._x),
+        _y(std::forward<Line>(l)._y){}
+    Line & operator=(Line const &  l)
+    {
+        _x = l._x;
+        _y = l._y;
+        return *this;
+    }
+    Line & operator=(Line       && l)
+    {
+        _x = std::forward<Line>(l)._x;
+        _y = std::forward<Line>(l)._y;
+        return *this;
+    }
+    Line(size_t size):_x(size),_y(size){}
+    ~Line(){}
+    size_t size() const
+    {
+        return _x.size();
+    }
+    const float & x(size_t it) const
+    {
+        return _x.at(it);
+    }
+    float & x(size_t it)
+    {
+        return _x.at(it);
+    }
+    const float & y(size_t it) const
+    {
+        return _y.at(it);
+    }
+    float & y(size_t it)
+    {
+        return _y.at(it);
+    }
+};
