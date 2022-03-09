a7ec909937fd98b6edada15b45991081fbb333c5
林恩衍 <ianlienfa@140-113-67-30.Dorm8.NCTU.edu.tw>
2022-03-09 22:34:34 +0800

    ianlienfa turns in hw2 [attempt-3]

---
+++ hw2/ianlienfa/q2/CMakeFiles/Vector.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Vector.dir/_vector.cpp.o"
+  "CMakeFiles/Vector.dir/_vector.cpp.o.d"
+  "libVector.a"
+  "libVector.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Vector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
