6d5467761a8b8e1ea0f0b8a0793e2ef8248a9361
林恩衍 <ianlienfa@140-113-67-30.Dorm8.NCTU.edu.tw>
2022-03-09 22:05:52 +0800

    ianlienfa turns in hw2 [attempt-1]

---
+++ hw2/ianlienfa/q2/CMakeFiles/Vector.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Vector.
+# This may be replaced when dependencies are built.
---
+++ hw2/ianlienfa/q2/CMakeFiles/_vector.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_vector.dir/_vector.cpp.o"
+  "CMakeFiles/_vector.dir/_vector.cpp.o.d"
+  "_vector.cpython-39-darwin.so"
+  "_vector.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/_vector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
