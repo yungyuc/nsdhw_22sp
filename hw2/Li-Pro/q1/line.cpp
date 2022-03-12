e78069d01415ecf7496d703e4eb6fe0cd6571f66
Li-Pro <chris.chou.c@nycu.edu.tw>
2022-03-13 05:34:03 +0800

    q1: implement Line member funcs.

--- hw2/Li-Pro/q1/line.cpp
+++ hw2/Li-Pro/q1/line.cpp
@@ -1,3 +1,5 @@
+#include <cassert>
+
 #include "line.hpp"
 
 Line::Line (size_t size)
@@ -7,29 +9,31 @@ Line::Line (size_t size)
 size_t
 Line::size() const
 {
+    assert( m_xvec.size() == m_yvec.size() );
     
+    return m_xvec.size();
 }
 
 float const &
 Line::x(size_t idx) const
 {
-    
+    return m_xvec[idx];
 }
 
 float &
 Line::x(size_t idx)
 {
-    
+    return m_xvec[idx];
 }
 
 float const &
 Line::y(size_t idx) const
 {
-
+    return m_yvec[idx];
 }
 
 float &
 Line::y(size_t idx)
 {
-
+    return m_yvec[idx];
 }
