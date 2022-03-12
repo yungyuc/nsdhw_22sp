a159d1c8b715c59f1c1b14265286633664da4cf4
Li-Pro <chris.chou.c@nycu.edu.tw>
2022-03-13 00:25:15 +0800

    Add q1 prototype.

---
+++ hw2/Li-Pro/q1/line.cpp
@@ -0,0 +1,29 @@
+#include "line.hpp"
+
+Line::Line (size_t size)
+    : m_xvec(size), m_yvec(size)
+{}
+
+float const &
+Line::x(size_t idx) const
+{
+    
+}
+
+float &
+Line::x(size_t idx)
+{
+    
+}
+
+float const &
+Line::y(size_t idx) const
+{
+
+}
+
+float &
+Line::y(size_t idx)
+{
+
+}
---
+++ hw2/Li-Pro/q1/line.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <vector>
+
+/*
+Container for 2D float points.
+Default destructed & copy constructed, thanks to STL containers.
+*/
+class Line
+{
+public:
+    Line(size_t size);
+
+    float const & x(size_t idx) const;
+    float       & x(size_t idx)      ;
+    float const & y(size_t idx) const;
+    float       & y(size_t idx)      ;
+
+private:
+    std::vector<float> m_xvec;
+    std::vector<float> m_yvec;
+};
