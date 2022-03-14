ff5e8bdbb0b86960d0afe271ea129b92c170ed34
minchuncho <violacho1002@gmail.com>
2022-03-14 14:45:45 +0800

    [hw2] question 2: fix indentation error

--- hw2/minchuncho/q2/test_app.py
+++ hw2/minchuncho/q2/test_app.py
@@ -8,6 +8,6 @@ def test_forty_five_degree():
     assert int(v1.angle(v1, v2)) == 45
 
 def test_ninety_degree():
-	v1 = _vector.Vec(1, 0);
-	v2 = _vector.Vec(0, 1);
+	v1 = _vector.Vec(1, 0)
+	v2 = _vector.Vec(0, 1)
     assert int(v1.angle(v1, v2)) == 90
