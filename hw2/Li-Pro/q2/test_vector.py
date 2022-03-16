52b26d6a7e67bb7d92d40d4e25c24fdc8bf65670
Li-Pro <chris.chou.c@nycu.edu.tw>
2022-03-17 01:48:52 +0800

    q1: add straight angle & common angle tests.

--- hw2/Li-Pro/q2/test_vector.py
+++ hw2/Li-Pro/q2/test_vector.py
@@ -34,10 +34,33 @@ def test_getRad_zero_length_raise_ValueError():
         getRad((0, 0), (0, 0))
 
 def test_getRad_zero_angle():
-    assert(_feq(getRad((1, 2,), (1, 2)), 0))
-    assert(_feq(getRad((2, 3,), (4, 6)), 0))
-    assert(_feq(getRad((3, -5,), (-3, 5)), 0))
+    assert(_feq(getRad((1, 2), (1, 2)), 0))
+    assert(_feq(getRad((2, 3), (4, 6)), 0))
+    assert(_feq(getRad((-3, 5), (-9, 15)), 0))
 
 def test_getRad_right_angle():
     assert(_feq(getRad((0, 1), (2, 0)), PI/2))
-    # assert(_feq(getRad((2, 3,), (4, 6)), PI/2))
+    assert(_feq(getRad((1.4142135, 1.4142135), (-1.4142135, 1.4142135)), PI/2))
+
+    for i in range(30):
+        base = 2 * PI * (i/30)
+        avec = (math.cos(base), math.sin(base))
+        bvec = (math.cos(base + PI/2), math.sin(base + PI/2))
+        assert(_feq(getRad(avec, bvec), PI/2))
+
+def test_getRad_straight_angle():
+    assert(_feq(getRad((0, 1), (0, -2)), 0))
+    assert(_feq(getRad((3, 0), (-4, 0)), 0))
+    assert(_feq(getRad((3, -5), (-3, 5)), 0))
+
+    for i in range(30):
+        base = 2 * PI * (i/30)
+        avec = (math.cos(base), math.sin(base))
+        bvec = (-math.cos(base), -math.sin(base))
+        assert(_feq(getRad(avec, bvec), 0))
+
+def test_getRad_common_angle():
+    assert(_feq(getRad((1.7320508, 0), (1.7320508, 1)), PI/6))
+    assert(_feq(getRad((1, 0), (1, 1.7320508)), PI/3))
+    assert(_feq(getRad((4, 0), (4, 3)), 0.6435011))
+
