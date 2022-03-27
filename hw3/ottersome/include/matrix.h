9bdb8e3904346403987bbff14e13870ca0f9c889
Luis Garcia <luisgarcia26284@gmail.com>
2022-03-27 16:59:25 +0800

    Adding basic files

--- hw3/ottersome/include/matrix.h
+++ hw3/ottersome/include/matrix.h
@@ -1,28 +1,86 @@
-#include "matrix.h"
+#include <stdio.h>
+#include <vector>
 
-Matrix operator*(Matrix const & mat1, Matrix const & mat2)
-{
-    if (mat1.ncol() != mat2.nrow())
-    {
-        throw std::out_of_range(
-            "the number of first matrix column "
-            "differs from that of second matrix row");
-    }
+class Matrix {
 
-    Matrix ret(mat1.nrow(), mat2.ncol());
+public:
 
-    for (size_t i=0; i<ret.nrow(); ++i)
-    {
-        for (size_t k=0; k<ret.ncol(); ++k)
-        {
-            double v = 0;
-            for (size_t j=0; j<mat1.ncol(); ++j)
-            {
-                v += mat1(i,j) * mat2(j,k);
-            }
-            ret(i,k) = v;
-        }
-    }
+    Matrix(size_t nrow, size_t ncol);
+    Matrix(size_t nrow, size_t ncol, std::vector<double> const & vec);
 
-    return ret;
-}
+    Matrix & operator=(std::vector<double> const & vec);
+    Matrix(Matrix const & other);
+    Matrix & operator=(Matrix const & other);
+    Matrix(Matrix && other);
+
+    Matrix & operator=(Matrix && other);
+    ~Matrix();
+
+    double   operator() (size_t row, size_t col) const;
+    double & operator() (size_t row, size_t col);
+
+    size_t nrow() const;
+    size_t ncol() const;
+
+    size_t size() const;
+    double buffer(size_t i) const;
+    std::vector<double> buffer_vector() const;
+
+    //Return pointer to Data
+    double * data();
+private:
+
+    size_t index(size_t row, size_t col) const;
+
+    void reset_buffer(size_t nrow, size_t ncol);
+
+    size_t m_nrow = 0;
+    size_t m_ncol = 0;
+    double * m_buffer = nullptr;
+
+};
+
+/*
+ * Naive matrix matrix multiplication.
+ */
+//Matrix operator*(Matrix const & mat1, Matrix const & mat2)
+//{
+    //if (mat1.ncol() != mat2.nrow())
+    //{
+        //throw std::out_of_range(
+            //"the number of first matrix column "
+            //"differs from that of second matrix row");
+    //}
+
+    //Matrix ret(mat1.nrow(), mat2.ncol());
+
+    //for (size_t i=0; i<ret.nrow(); ++i)
+    //{
+        //for (size_t k=0; k<ret.ncol(); ++k)
+        //{
+            //double v = 0;
+            //for (size_t j=0; j<mat1.ncol(); ++j)
+            //{
+                //v += mat1(i,j) * mat2(j,k);
+            //}
+            //ret(i,k) = v;
+        //}
+    //}
+
+    //return ret;
+//}
+
+//std::ostream & operator << (std::ostream & ostr, Matrix const & mat)
+//{
+    //for (size_t i=0; i<mat.nrow(); ++i)
+    //{
+        //ostr << std::endl << " ";
+        //for (size_t j=0; j<mat.ncol(); ++j)
+        //{
+            //ostr << " " << std::setw(2) << mat(i, j);
+        //}
+    //}
+
+    //return ostr;
+//}
+
--- hw3/ottersome/src/main.cpp
+++ hw3/ottersome/src/main.cpp
@@ -1,23 +1,54 @@
 #include "main.h"
 #include "matrix.h"
 
-int main(int argc, char ** argv)
-{
-    std::cout << ">>> A(2x3) times B(3x2):" << std::endl;
-    Matrix mat1(2, 3, std::vector<double>{1, 2, 3, 4, 5, 6});
-    Matrix mat2(3, 2, std::vector<double>{1, 2, 3, 4, 5, 6});
+#include <pybind11/pybind11.h>
 
-    Matrix mat3 = mat1 * mat2;
+namespace py = pybind11;
 
-    std::cout << "matrix A (2x3):" << mat1 << std::endl;
-    std::cout << "matrix B (3x2):" << mat2 << std::endl;
-    std::cout << "result matrix C (2x2) = AB:" << mat3 << std::endl;
 
-    std::cout << ">>> B(3x2) times A(2x3):" << std::endl;
-    Matrix mat4 = mat2 * mat1;
-    std::cout << "matrix B (3x2):" << mat2 << std::endl;
-    std::cout << "matrix A (2x3):" << mat1 << std::endl;
-    std::cout << "result matrix D (3x3) = BA:" << mat4 << std::endl;
+PYBIND11_MODULE(example, m) {
+    //py::class_<Animal>(m, "Animal")
+    //.def("go", &Animal::go);
 
-    return 0;
+    //py::class_<Dog, Animal>(m, "Dog")
+    //.def(py::init<>());
+
+    //m.def("call_go", &call_go);
+
+    py::class_<Matrix>(m, "Matrix", py::buffer_protocol())
+        .def_buffer([](Matrix &m) -> py::buffer_info {
+                return py::buffer_info(
+                        m.data(),                               /* Pointer to buffer */
+                        sizeof(float),                          /* Size of one scalar */
+                        py::format_descriptor<float>::format(), /* Python struct-style format descriptor */
+                        2,                                      /* Number of dimensions */
+                        { m.nrow(), m.ncol() },                 /* Buffer dimensions */
+                        { sizeof(float) * m.ncol(),             /* Strides (in bytes) for each index */
+                        sizeof(float) }
+                        );
+                });
 }
+
+
+
+
+//int main(int argc, char ** argv)
+//{
+    //std::cout << ">>> A(2x3) times B(3x2):" << std::endl;
+    //Matrix mat1(2, 3, std::vector<double>{1, 2, 3, 4, 5, 6});
+    //Matrix mat2(3, 2, std::vector<double>{1, 2, 3, 4, 5, 6});
+
+    //Matrix mat3 = mat1 * mat2;
+
+    //std::cout << "matrix A (2x3):" << mat1 << std::endl;
+    //std::cout << "matrix B (3x2):" << mat2 << std::endl;
+    //std::cout << "result matrix C (2x2) = AB:" << mat3 << std::endl;
+
+    //std::cout << ">>> B(3x2) times A(2x3):" << std::endl;
+    //Matrix mat4 = mat2 * mat1;
+    //std::cout << "matrix B (3x2):" << mat2 << std::endl;
+    //std::cout << "matrix A (2x3):" << mat1 << std::endl;
+    //std::cout << "result matrix D (3x3) = BA:" << mat4 << std::endl;
+
+    //return 0;
+//}
