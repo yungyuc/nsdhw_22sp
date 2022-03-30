ab39c04459139e0551f91cb0385010698e2f0979
minchuncho <violacho1002@gmail.com>
2022-03-30 17:59:38 +0800

    [hw3] finished the matrix mul functions and the unit test
    1. export specific functions to pass validate.sh
    2. fixed error: shouldn't have freed the pointers in multiply_mkl

---
+++ hw3/minchuncho/matrix.cpp
@@ -0,0 +1,201 @@
+//
+//  matrix.cpp
+//  nsd-hw3
+//
+//  Created by MinChunCho on 2022/3/25.
+//
+
+#include "matrix.hpp"
+
+Matrix::Matrix(size_t const& row, size_t const& col)
+    : row_(row), col_(col)
+{
+    this->vec_.clear();
+    this->vec_.resize(row*col, 0);
+}
+
+Matrix::Matrix(size_t const& row, size_t const& col, std::vector<double> const& vec)
+    :row_(row), col_(col)
+{
+    if(vec.size() != row*col) throw std::out_of_range("size mismatches");
+    this->vec_ = vec;
+}
+
+// copy constructor
+Matrix::Matrix(Matrix const& mat)
+{
+    this->row_ = mat.row_;
+    this->col_ = mat.col_;
+    this->vec_ = mat.vec_;
+}
+
+// copy assignment
+Matrix& Matrix::operator=(Matrix const& mat)
+{
+    if(this == &mat) return *this;
+    
+    this->row_ = mat.row_;
+    this->col_ = mat.col_;
+    this->vec_ = mat.vec_;
+    
+    return *this;
+}
+
+// move constructor
+Matrix::Matrix(Matrix && mat)
+{
+    std::swap(this->row_, mat.row_);
+    std::swap(this->col_, mat.col_);
+    this->vec_ = std::move(mat.vec_);
+}
+
+// move assignment
+Matrix& Matrix::operator=(Matrix && mat)
+{
+    if(this == &mat) return *this;
+    
+    std::swap(this->row_, mat.row_);
+    std::swap(this->col_, mat.col_);
+    this->vec_ = std::move(mat.vec_);
+    
+    return *this;
+}
+
+Matrix& Matrix::operator=(std::vector<double> const& vec)
+{
+    this->vec_ = vec;
+    return *this;
+}
+
+double& Matrix::operator()(size_t const& i, size_t const& j)
+{
+    return this->vec_[i*this->col_ + j];
+}
+
+double Matrix::operator()(size_t const& i, size_t const& j) const
+{
+    return this->vec_[i*this->col_ + j];
+}
+
+bool Matrix::operator==(Matrix const& mat) const
+{
+    if(this->col_ != mat.col_ || this->row_ != mat.row_) return false;
+    
+    for(size_t i=0; i<this->size(); i++){
+        // a floating-point precision issue
+        // if(this->vec_[i] != mat.vec_[i]) return false;
+        if(fabs(this->vec_[i]-mat.vec_[i]) > 0.000001f) return false;
+    }
+    
+    return true;
+}
+
+bool Matrix::bound_check(size_t const& i, size_t const& j)
+{
+    return i<this->row_ && i>=0 && j<this->col_ && j>=0;
+}
+
+double Matrix::get_element(std::pair<size_t, size_t> index)
+{
+    return (*this)(index.first, index.second);
+}
+
+void Matrix::set_element(std::pair<size_t, size_t> index, const double& item)
+{
+    (*this)(index.first, index.second) = item;
+}
+
+std::string Matrix::get_matrix_str()
+{
+    std::string s;
+    for(size_t i=0; i<row_; i++){
+        for(size_t j=0; j<col_; j++){
+            s += std::to_string((*this)(i, j)) + ' ';
+        }
+        s += '\n';
+    }
+    return s;
+}
+
+std::ostream& operator<<(std::ostream& os, Matrix mat)
+{
+    for(size_t i=0; i<mat.row_; i+=mat.col_){
+        for(size_t j=0; j<mat.col_; j++){
+            std::cout << mat(i, j) << " ";
+        }
+        std::cout << std::endl;
+    }
+    
+    return os;
+}
+
+Matrix multiply_naive(Matrix const& mat1, Matrix const& mat2)
+{
+    if(mat1.col() != mat2.row()){
+        throw std::out_of_range("mat1 col is different from mat2 row");
+    }
+
+    Matrix ret(mat1.row(), mat2.col());
+
+    for(size_t i=0; i<mat1.row(); i++){
+        for(size_t j=0; j<mat2.col(); j++){
+            double v = 0;
+            for(size_t k=0; k<mat1.col(); k++){
+                v += mat1(i, k)*mat2(k, j);
+            }
+            ret(i,j) = v;
+        }
+    }
+
+    return ret;
+}
+
+Matrix multiply_tile(Matrix const& mat1, Matrix const& mat2, size_t tsize)
+{
+    if(mat1.col() != mat2.row()){
+        throw std::out_of_range("mat1 col is different from mat2 row");
+    }
+
+    Matrix ret(mat1.row(), mat2.col());
+    
+    for(size_t ti=0; ti<mat1.row(); ti+=tsize){
+        for(size_t tj=0; tj<mat2.col(); tj+=tsize){
+            for(size_t tk=0; tk<mat1.col(); tk+=tsize){
+                size_t i_size = mat1.row()<ti+tsize ? mat1.row():ti+tsize;
+                for(size_t i=ti; i<i_size; i++){
+                    size_t j_size = mat2.col()<tj+tsize ? mat2.col():tj+tsize;
+                    for(size_t j=tj; j<j_size; j++){
+                        double v = 0;
+                        size_t k_size = mat1.col()<tk+tsize ? mat1.col():tk+tsize;
+                        for(size_t k=tk; k<k_size; k++){
+                            v += mat1(i, k)*mat2(k, j);
+                        }
+                        ret(i,j) += v;
+                    }
+                }
+
+            }
+        }
+    }
+    
+    return ret;
+}
+
+Matrix multiply_mkl(Matrix& mat1, Matrix& mat2)
+{
+    if(mat1.col() != mat2.row()){
+        throw std::out_of_range("mat1 col is different from mat2 row");
+    }
+
+    Matrix ret(mat1.row(), mat2.col());
+    
+    const size_t m = mat1.row(), n = mat2.col(), k = mat1.col();
+    double alpha = 1.0, beta = 0.0;
+    double* A = mat1.addr();
+    double* B = mat2.addr();
+    double* C = ret.addr();
+
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m, n, k, alpha, A, k, B, n, beta, C, n);
+
+    return ret;
+}
---
+++ hw3/minchuncho/matrix.hpp
@@ -0,0 +1,77 @@
+//
+//  matrix.hpp
+//  nsd-hw3
+//
+//  Created by MinChunCho on 2022/3/25.
+//
+
+#ifndef matrix_hpp
+#define matrix_hpp
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdexcept>
+#include <pybind11/pybind11.h>
+#include <pybind11/operators.h>
+#include <pybind11/stl.h>
+#include <mkl.h>
+
+namespace py = pybind11;
+
+class Matrix {
+public:
+    Matrix(size_t const& row, size_t const& col);
+    Matrix(size_t const& row, size_t const& col, std::vector<double> const& vec);
+    Matrix(Matrix const& mat);
+    Matrix& operator=(Matrix const& mat);
+    Matrix(Matrix && mat);
+    Matrix& operator=(Matrix && mat);
+    ~Matrix(){
+        vec_.clear();
+        vec_.shrink_to_fit();
+    }
+    
+    Matrix& operator=(std::vector<double> const& vec);
+    double& operator()(size_t const& i, size_t const& j);
+    double operator()(size_t const& i, size_t const& j) const;
+    bool operator==(Matrix const& mat) const;
+    friend std::ostream& operator<<(std::ostream& os, Matrix mat);
+
+    size_t row() const { return row_; }
+    size_t col() const { return col_; }
+    size_t size() const { return row_*col_; }
+    double* addr() { return &vec_[0]; }
+    double get_element(std::pair<size_t, size_t> index);
+    void set_element(std::pair<size_t, size_t> index, const double& item);
+    std::string get_matrix_str();
+
+private:
+    bool bound_check(size_t const& i, size_t const& j);
+
+    size_t row_;
+    size_t col_;
+    std::vector<double> vec_;
+};
+
+Matrix multiply_naive(Matrix const& ma, Matrix const& mb);
+Matrix multiply_tile(Matrix const& mat1, Matrix const& mat2, size_t tsize);
+Matrix multiply_mkl(Matrix& ma, Matrix& mb);
+
+PYBIND11_MODULE(_matrix, m) {
+    py::class_<Matrix>(m, "Matrix")
+        .def(py::init<size_t const&, size_t const&>())
+        .def(py::init<size_t const&, size_t const&, std::vector<double> const&>())
+        .def(py::self == py::self) // operator overloading
+        .def_property_readonly("nrow", &Matrix::row) // the following functions are made for validate.sh
+        .def_property_readonly("ncol", &Matrix::col)
+        .def("__setitem__", &Matrix::set_element)
+        .def("__getitem__", &Matrix::get_element)
+        .def("__repr__", &Matrix::get_matrix_str);
+        
+    m.def("multiply_naive", &multiply_naive);
+    m.def("multiply_tile", &multiply_tile);
+    m.def("multiply_mkl", &multiply_mkl);
+};
+
+#endif /* matrix_hpp */
