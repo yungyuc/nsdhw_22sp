132d0e28d3c330d255a0f5ec20ae563b0a094f3f
Li-Pro <chris.chou.c@nycu.edu.tw>
2022-03-31 00:19:18 +0800

    hw3: Use pybind11 for C++ binding and pytest for unit tests

--- hw3/Li-Pro/_matrix.py
+++ hw3/Li-Pro/_matrix.py
@@ -1,1 +1,12 @@
+#!/usr/bin/env python3
+"""
+Wrapper for libmatrix
+"""
+
 from libmatrix  import Matrix, multiply_naive, multiply_mkl
+
+def benchmark():
+    return
+
+if __name__ == "__main__":
+    benchmark()
