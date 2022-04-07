5a65e09106c4a16cc8f6fb8200c8e5818663428f
Li-Pro <chris.chou.c@nycu.edu.tw>
2022-04-07 21:01:23 +0800

    Implement python wrapper

--- hw3/Li-Pro/_matrix.py
+++ hw3/Li-Pro/_matrix.py
@@ -3,17 +3,22 @@
 Wrapper for libmatrix
 """
 
+from collections.abc  import Sequence
+
 from libmatrix  import _Matrix, multiply_naive, multiply_tile, multiply_mkl
 
-def Matrix(_Matrix):
-    def __getitem__(self, key):
-        return
+class Matrix(_Matrix):
+    def __getitem__(self, key: Sequence):
+        if isinstance(key, Sequence):
+            assert(len(key) == 2)
+            return _Matrix[key[0]][key[1]]
+        else:
+            # return _Matrix[key]
+            raise ValueError("key should be an sequence")
     
-    def __setitem__(self, key):
-        return
-
-def benchmark():
-    return
-
-if __name__ == "__main__":
-    benchmark()
+    def __setitem__(self, key: Sequence, value: float):
+        if isinstance(key, Sequence):
+            assert(len(key) == 2)
+            _Matrix[key[0]][key[1]] = value
+        else:
+            raise ValueError("key should be an sequence")
