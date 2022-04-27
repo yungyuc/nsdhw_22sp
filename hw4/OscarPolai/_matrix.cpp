34d5c0178b1de92e335c80ec544e7fb1fab07377
Ubuntu <ubuntu@ip-172-31-15-91.ap-northeast-1.compute.internal>
2022-04-27 16:32:44 +0000

    Remodify _matrix.cpp

--- hw4/OscarPolai/_matrix.cpp
+++ hw4/OscarPolai/_matrix.cpp
@@ -17,89 +17,81 @@ struct ByteCounterImpl{
     std::atomic_size_t deallocated{0};
 }; 
 
-class ByteCounter {
- public:
- 	ByteCounter() : m_allocated(0), m_deallocated(0) {};
- 	ByteCounter(ByteCounter const& other) : m_allocated(other.m_allocated), m_deallocated(other.m_deallocated) {};
- 	ByteCounter(ByteCounter && other)
-     {
-         std::swap(m_allocated, other.m_allocated);
-         std::swap(m_deallocated, other.m_deallocated);
-     }
- 	ByteCounter& operator=(ByteCounter const& other)
-     {
-         if (&other != this)
-         {
-             m_allocated = other.m_allocated;
-             m_deallocated = other.m_deallocated;
-         }
-         return *this;
-     }
- 	ByteCounter& operator=(ByteCounter && other)
-     {
-         if (&other != this)
-         {
-             std::swap(m_allocated, other.m_allocated);
-             std::swap(m_deallocated, other.m_deallocated);
-         }
-         return *this;
-     }
- 	~ByteCounter() {};
+class ByteCounter{
 
- 	void increase(size_t bytes) { m_allocated += bytes; }
- 	void decrease(size_t bytes) { m_deallocated += bytes; }
+public:
+    
+    ByteCounter(): m_impl(new ByteCounterImpl){}
+    void swap(ByteCounter & other);
+    void increase(std::size_t amount);
+    void decrease(std::size_t amount);
+    std::size_t bytes() const;
+    std::size_t allocated() const;
+    std::size_t deallocated() const;
+    
+private:
+    ByteCounterImpl * m_impl;
+};
 
- 	size_t bytes() const { return m_allocated - m_deallocated; };
- 	size_t allocated() const { return m_allocated; };
- 	size_t deallocated() const { return m_deallocated; };
+void ByteCounter::swap(ByteCounter & other){
+        std::swap(m_impl, other.m_impl);
+}
 
- private:
-     size_t m_allocated;
-     size_t m_deallocated;
- };
+void ByteCounter::increase(std::size_t amount){
+        m_impl->allocated += amount;
+}
 
+void ByteCounter::decrease(std::size_t amount){
+        m_impl->deallocated += amount;
+}
 
-template<class T>
- class Allocator {
- public:
- 	typedef T value_type;
+std::size_t ByteCounter::bytes() const { return m_impl->allocated - m_impl->deallocated; }
+std::size_t ByteCounter::allocated() const { return m_impl->allocated; }
+std::size_t ByteCounter::deallocated() const { return m_impl->deallocated; }
 
-     Allocator() = default;
+template <class T>
+struct Allocator
+{
+    using value_type = T;
 
- 	T* allocate(size_t n)
-     {
-         if (n > std::numeric_limits<size_t>::max()/sizeof(T))
-         {
-             throw std::bad_alloc();
-         }
+    Allocator() = default;
 
-         const size_t bytes = n*sizeof(T);
+    template <class U> constexpr
+    Allocator(const Allocator<U> & other) noexcept
+    {
+        counter = other.counter;
+    }
 
-         T* p = static_cast<T*>(std::malloc(bytes));
-         if (p)
-         {
-             counter.increase(bytes);
-             return p;
-         }
-         else
-         {
-             throw std::bad_alloc();
-         }
-     }
+    T * allocate(std::size_t n)
+    {
+        if (n > std::numeric_limits<std::size_t>::max() / sizeof(T))
+        {
+            throw std::bad_alloc();
+        }
+        const std::size_t bytes = n*sizeof(T);
+        T * p = static_cast<T *>(std::malloc(bytes));
+        if (p)
+        {
+            counter.increase(bytes);
+            return p;
+        }
+        else
+        {
+            throw std::bad_alloc();
+        }
+    }
 
-     void deallocate(T* p, size_t n) noexcept
-     {
-         std::free(p);
+    void deallocate(T* p, std::size_t n) noexcept
+    {
+        std::free(p);
 
-         const size_t bytes = n * sizeof(T);
-         counter.decrease(bytes);
-     }
+        const std::size_t bytes = n*sizeof(T);
+        counter.decrease(bytes);
+    }
 
-     static ByteCounter counter;
- };
+    ByteCounter counter;
+};
 
- template <class T>
- ByteCounter Allocator<T>::counter{};
 
  template <class T, class U>
  bool operator==(const Allocator<T> & a, const Allocator<U> & b)
@@ -114,209 +106,163 @@ template<class T>
  } 
 
 
+template <class T>
+std::ostream & operator << (std::ostream & out, const Allocator<T> & alloc)
+{
+    out << "allocator: bytes = " << alloc.counter.bytes();
+    out << " allocated = " << alloc.counter.allocated();
+    out << " deallocated = " << alloc.counter.deallocated();
+    return out;
+}
+
+
 Allocator<double> alloc;
 
- class Matrix
- {
+size_t bytes(){
+     return alloc.counter.bytes();
+}
+
+size_t allocated(){
+     return alloc.counter.allocated();
+}
+
+size_t deallocated(){
+     return alloc.counter.deallocated();
+}
+
+class Matrix{
  public:
-     Matrix(size_t row, size_t col) : m_rows(row), m_cols(col), m_vec(alloc)
-     {
-         m_vec.clear();
-         m_vec.resize(row * col, 0);
-     }
+    Matrix(size_t row, size_t col);
+    ~Matrix();
+    Matrix(Matrix &&other);
+     // getter
+    const size_t nrow() const;
+    const size_t ncol() const;
+    double *addr();
+    
+     // rewrite operator
+    double operator()(size_t row, size_t col) const;
+    double &operator()(size_t row, size_t col);
+    bool operator==(const Matrix &other) const;
 
-     // copy constructor
-     Matrix(Matrix const &other)
-     {
-         this->m_rows = other.m_rows;
-         this->m_cols = other.m_cols;
-         this->m_vec = other.m_vec;
-     }
+    double getitem(std::pair<size_t, size_t> index);
+    void setitem(std::pair<size_t, size_t> index, const double value);
+    
+    std::string str() const;
 
+private:
+    std::vector<double, Allocator<double>> m_vec;
+    size_t m_rows;
+    size_t m_cols;
+};
+
+
+Matrix::Matrix(size_t row, size_t col) : m_rows(row), m_cols(col), m_vec(alloc){
+    m_vec.clear();
+    m_vec.resize(row * col, 0);
+}
+
+Matrix::~Matrix(){
+    m_vec.clear();
+    m_vec.shrink_to_fit();
+}
      // move constructor
-     Matrix(Matrix &&other) : m_vec(std::move(other.m_vec))
+Matrix::Matrix(Matrix &&other) : m_vec(std::move(other.m_vec))
      {
          std::swap(this->m_rows, other.m_rows);
          std::swap(this->m_cols, other.m_cols);
      }
+     
 
-     // copy assignment operator
-     Matrix &operator=(Matrix const &other)
-     {
-         if (this != &other)
-         {
-             this->m_rows = other.m_rows;
-             this->m_cols = other.m_cols;
-             this->m_vec = other.m_vec;
-         }
-         return *this;
-     }
+const size_t Matrix::nrow() const{ return m_rows;}
+const size_t Matrix::ncol() const{ return m_cols;} 
 
-     // move assignment operator
-     Matrix &operator=(Matrix &&other)
-     {
-         if (this != &other)
-         {
-             this->m_rows = other.m_rows;
-             this->m_cols = other.m_cols;
-             std::move(other.m_vec.begin(), other.m_vec.end(), std::back_inserter(this->m_vec));
-         }
-         return *this;
-     }
-
-     // destructor
-     ~Matrix()
-     {
-         m_vec.clear(); 
-         m_vec.shrink_to_fit();
-     }
-
-     // getter
-     const size_t nrow() const
-     {
-         return m_rows;
-     }
-     const size_t ncol() const
-     {
-         return m_cols;
-     }
-     double *addr()
-     {
-         return &m_vec[0];
-     }
-
-
-     // rewrite operator
-     double operator()(size_t row, size_t col) const
-     {
+double *Matrix::addr(){ return &m_vec[0];}
+ 
+double Matrix::operator()(size_t row, size_t col) const{
          return m_vec[m_cols * row + col];
-     }
-     double &operator()(size_t row, size_t col)
-     {
+}
+
+double& Matrix::operator()(size_t row, size_t col){
          return m_vec[m_cols * row + col];
-     }
-     bool operator==(const Matrix &other) const
-     {
-         if (m_rows != other.nrow() || m_cols != other.ncol())
-             return false;
+}
 
-         for (size_t i = 0; i < m_rows; ++i)
-             for (size_t j = 0; j < m_cols; ++j)
-                 if ((*this)(i, j) != other(i, j))
-                     return false;
+bool Matrix::operator==(const Matrix &other) const{
+    if (m_rows != other.nrow() || m_cols != other.ncol()) return false;
 
-         return true;
-     }
+    for (size_t i = 0; i < m_rows; ++i){
+        for (size_t j = 0; j < m_cols; ++j){
+            if ((*this)(i, j) != other(i, j)) return false;
+        }
+    }
+    return true;
+}
 
-     double getitem(std::pair<size_t, size_t> index)
-     {
-         return (*this)(index.first, index.second);
-     }
+double Matrix::getitem(std::pair<size_t, size_t> index){
+        return (*this)(index.first, index.second);
+}
 
-     void setitem(std::pair<size_t, size_t> index, const double value)
-     {
-         (*this)(index.first, index.second) = value;
-     }
-     std::string str() const
-     {
-	 std::string s = "";
-         for (size_t i = 0; i < m_rows; ++i)
-         {
-             for (size_t j = 0; j < m_cols; ++j)
-                 s += std::to_string(m_vec[i * m_cols + j]) + ' ';
-             s += '\n';
-         }
-         return s;
-     }
+void Matrix::setitem(std::pair<size_t, size_t> index, const double value){
+        (*this)(index.first, index.second) = value;
+}
+ 
+std::string Matrix::str() const{
+        std::string s = "";
+        for (size_t i = 0; i < m_rows; ++i){
+            for (size_t j = 0; j < m_cols; ++j)
+                s += std::to_string(m_vec[i * m_cols + j]) + ' ';
+            s += '\n';
+        }
+        return s;
+}
 
- private:
-     std::vector<double, Allocator<double>> m_vec;
-     size_t m_rows;
-     size_t m_cols;
- };
+void is_validate_matrix(Matrix const &m1, Matrix const &m2){
+    if (m1.ncol() != m2.nrow())
+        throw std::out_of_range("The columns of matrix1 must be equal to the rows of matrix2.");
+}
 
- size_t bytes()
- {
-     return alloc.counter.bytes();
- }
+Matrix multiply_naive(Matrix const &m1, Matrix const &m2){
+    is_validate_matrix(m1, m2);
 
- size_t allocated()
- {
-     return alloc.counter.allocated();
- }
+    Matrix ret(m1.nrow(), m2.ncol());
 
- size_t deallocated()
- {
-     return alloc.counter.deallocated();
- }
-
-
- void is_validate_matrix(Matrix const &m1, Matrix const &m2)
- {
-     if (m1.ncol() != m2.nrow())
-         throw std::out_of_range("The columns of matrix1 must be equal to the rows of matrix2.");
- }
-
- Matrix multiply_naive(Matrix const &m1, Matrix const &m2)
- {
-     is_validate_matrix(m1, m2);
-
-     Matrix ret(m1.nrow(), m2.ncol());
-
-     for (size_t i = 0; i < m1.nrow(); ++i)
-     {
-         for (size_t k = 0; k < m2.ncol(); ++k)
-         {
-             double v = 0;
-             for (size_t j = 0; j < m1.ncol(); ++j)
-             {
+    for (size_t i = 0; i < m1.nrow(); ++i){
+        for (size_t k = 0; k < m2.ncol(); ++k){
+            double v = 0;
+            for (size_t j = 0; j < m1.ncol(); ++j){
                  v += m1(i, j) * m2(j, k);
-             }
-             ret(i, k) = v;
-         }
-     }
-
-     return ret;
+            }
+            ret(i, k) = v;
+        }
+    }
+    return ret;
  }
 
- Matrix multiply_tile(Matrix const &m1, Matrix const &m2, size_t tsize)
- {
-     is_validate_matrix(m1, m2);
+Matrix multiply_tile(Matrix const &m1, Matrix const &m2, size_t tsize){
+    is_validate_matrix(m1, m2);
 
-     Matrix ret(m1.nrow(), m2.ncol());
+    Matrix ret(m1.nrow(), m2.ncol());
 
-     for (size_t ti = 0; ti < m1.nrow(); ti += tsize)
-     {
-         size_t imax = std::min(ti + tsize, m1.nrow());
-
-         for (size_t tj = 0; tj < m2.ncol(); tj += tsize)
-         {
-             size_t jmax = std::min(tj + tsize, m2.ncol());
-
-             for (size_t tk = 0; tk < m1.ncol(); tk += tsize)
-             {
+    for (size_t ti = 0; ti < m1.nrow(); ti += tsize){
+        size_t imax = std::min(ti + tsize, m1.nrow());
+        for (size_t tj = 0; tj < m2.ncol(); tj += tsize){
+            size_t jmax = std::min(tj + tsize, m2.ncol());
+            for (size_t tk = 0; tk < m1.ncol(); tk += tsize){
                  size_t kmax = std::min(tk + tsize, m1.ncol());
-
-                 for (size_t k = tk; k < kmax; ++k)
-                 {
-                     for (size_t i = ti; i < imax; ++i)
-                     {
-                         for (size_t j = tj; j < jmax; ++j)
-                         {
+                for (size_t k = tk; k < kmax; ++k){
+                    for (size_t i = ti; i < imax; ++i){
+                        for (size_t j = tj; j < jmax; ++j){
                              ret(i, j) += m1(i, k) * m2(k, j);
-                         }
-                     }
-                 }
-             }
-         }
-     }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return ret;
+}
 
-     return ret;
- }
-
- // Computing matrix product using Intel(R) MKL dgemm function via CBLAS interface
- // Ref: https://www.intel.com/content/www/us/en/develop/documentation/mkl-tutorial-c/top/multiplying-matrices-using-dgemm.html#multiplying-matrices-using-dgemm
- Matrix multiply_mkl(Matrix &m1, Matrix &m2)
+Matrix multiply_mkl(Matrix &m1, Matrix &m2)
  {
      is_validate_matrix(m1, m2);
 
@@ -356,7 +302,6 @@ PYBIND11_MODULE(_matrix, m)
      m.def("allocated", &allocated);
      m.def("deallocated", &deallocated);
 
-     // Ref: https://pybind11.readthedocs.io/en/stable/advanced/classes.html
      pybind11::class_<Matrix>(m, "Matrix")
          .def(pybind11::init<size_t, size_t>()) // call the constructor
          .def(pybind11::self == pybind11::self)       // operator overloading (==)
@@ -367,6 +312,26 @@ PYBIND11_MODULE(_matrix, m)
          .def("__repr__", &Matrix::str);
  } 
 
+/*
+PYBIND11_MODULE(_matrix, m){
+     m.doc() = "matrix C++ implementation";
+     
+     m.def("multiply_naive", &multiply_naive);
+     m.def("multiply_tile", &multiply_tile);
+     m.def("multiply_mkl", &multiply_mkl);
+
+     m.def("bytes", &bytes);
+     m.def("allocated", &allocated);
+     m.def("deallocated", &deallocated);
+
+     pybind11::class_<Matrix>(m, "Matrix")
+	     .def(pybind11::init<size_t, size_t>())
+             .def("__getitem__", &Matrix::getitem)
+	     .def("__setitem__", &Matrix::setitem)
+             .def("__repr__", &Matrix::str)
+     	     .def_property_readonly("nrow", &Matrix::nrow)
+    	     .def_property_readonly("ncol", &Matrix::ncol);
+ }*/ 
 /*
 int main(){
     //std::vector<double, MyAllocator<double>> m_vec = vector<double, MyAllocator<double>>(alloc);
